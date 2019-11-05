561a035509de058255562ecca6145d3c8274bdb7
fedegiuliano <fedegiuliano11@gmail.com>
2019-11-05 14:57:41 -0300

    agrego

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# probando
+# Filmster
+
+Aplicación utilizada en la cursada 2019 de Ingenieria de Software. UTN-FRLP
+
+## Dependencias
+
+ - node > v10
+ - express > v4
+
+## Instalar dependencias
+
+`npm install`
+
+## Iniciar la app en modo desarrollo
+
+`npm run dev`
+
+## Iniciar la app en modo producción
+
+`npm start`
+<h1>
+	Somos el grupo numero 7
+</h1>
+<h1>
+	Giuliano de la Vega,Federico
+</h1>
+<h1>
+	Rodriguez Maximiliano
+</h1>
+<h1> 
+	Renaldi Jeronimo
+</h1>
---
+++ server/src/models/movie.js
@@ -0,0 +1,79 @@
+const Sequelize = require('sequelize')
+
+const db = require('../db.js')
+
+const Movie = db.define('Movie', {
+	// attributes
+	title: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	description: {
+		type: Sequelize.STRING
+	},
+	year: {
+		type: Sequelize.INTEGER,
+		allowNull: false
+	},
+	runtime: {
+		type: Sequelize.INTEGER,
+		allowNull: false
+	},
+	country: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	language: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	genres: {
+		type: Sequelize.JSON
+	},
+	directors: {
+		type: Sequelize.JSON
+	},
+	writers: {
+		type: Sequelize.JSON
+	}
+}, {
+  tableName: 'Movie'
+})
+
+const getAllMovies = () => Movie.findAll().then(movies => movies)
+
+const createMovie = (data) =>
+	Movie.sync().then(() =>
+		Movie.create(data)
+	)
+
+const getMovie = (id) => Movie.findOne({where: {id: id}}).then(movie => movie)
+
+const updateMovie = (id, data) => {
+	return Movie.findOne({where: {id: id}}).then(movie => {
+		if (movie != null) {
+			return movie.update(data).then(d => d)
+		}
+		return null
+	})	
+}
+
+const deleteMovie = (id) => {
+	return Movie.findOne({where: {id: id}}).then(movie => {
+		if (movie != null) {
+			return movie.destroy()
+		}
+		return null
+	})	
+}
+
+const MovieModel = {
+	Movie: Movie,
+	getAll: getAllMovies,
+	create: createMovie,
+	get: getMovie,
+	update: updateMovie,
+	delete: deleteMovie
+}
+
+module.exports = MovieModel
